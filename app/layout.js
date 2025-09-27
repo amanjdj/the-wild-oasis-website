e5752dcc479d2aace5d32c72a9adfd649fbcb380
Aman Sharma <amanjdj@gmail.com>
2025-09-27 21:32:08 +0530

    Basic Layout setup

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,14 @@
-import { title } from "process";
+import Header from "./_components/Header";
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   // title: "The Wild Oasis",
@@ -9,22 +16,21 @@ export const metadata = {
     template: "%s / The Wild Oasis",
     default: "Welcome / The Wild Oasis",
   },
+  description:
+    "Luxurious cabin hotel, located in the heart of mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className=" bg-primary-950 text-primary-100
-       min-h-screen "
+        className={` ${josefin.className} bg-primary-950 text-primary-100
+       min-h-screen flex flex-col antialiased relative`}
       >
-        <header>
-          <Logo />
-
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by Aman</footer>
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
       </body>
     </html>
   );
